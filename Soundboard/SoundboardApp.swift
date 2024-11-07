f4a819a4f77849fa36cec7ee6c2ba45da96255cf
Liam Seymour <spaceturatle@gmail.com>
2024-11-07 08:52:42 -0600

    Initial Commit

---
+++ Soundboard/SoundboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundboardApp.swift
+//  Soundboard
+//
+//  Created by Liam K. Seymour on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
